d55c4d29f34e174981fb5ce0d346edc898b7f884
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-06-30 15:56:41 +0300

    Update __init__.py

--- apps/tools/background image/__init__.py
+++ apps/tools/background image/__init__.py
@@ -1,4 +1,4 @@
 def load(manager, params):
     pos = (30, 30);import pygame;import pygame_gui;import os
     if params is not None and len(params) > 0:pos = params[0]
-    pygame_gui.windows.UIFileDialog(rect=pygame.Rect(pos, (600, 400)),manager=manager,window_title="Set Background Image",initial_file_path='\\'.join(os.path.dirname(os.path.realpath(__file__)).split('\\')[:-3])+'/photos',object_id="#background_picker",)
+    pygame_gui.windows.UIFileDialog(rect=pygame.Rect(pos, (600, 400)),manager=manager,window_title="Set Background Image",initial_file_path='\\'.join(os.path.dirname(os.path.realpath(__file__)).split('\\')[:-3])+'/images',object_id="#background_picker",)
