1ac5cfc96d75d899f285b4d95a332542c80b128b
Elisha Hollander <just4now666666@gmail.com>
2020-08-25 15:06:43 +0300

    Update __init__.py

--- apps/util/maze/__init__.py
+++ apps/util/maze/__init__.py
@@ -1,6 +1,56 @@
 from random import randint;import pygame;from pygame_gui.elements.ui_window import UIWindow;from pygame_gui.elements.ui_image import UIImage;import os;from math import cos, sin, tan, pi, floor, ceil, sqrt;two_pi = pi * 2;half_pi = pi * 0.5;three_half_pi = pi * 1.5;NUMBER_OF_RAYS = 320
+class DisjointSet:
+	class Element:
+		def __init__(self, key):self.key = key;self.parent = self;self.rank = 0
+		def __eq__(self, other):return self.key ==  other.key
+		def __ne__(self, other):return self.key != other.key
+	def __init__(self):self.tree = {}
+	def make_set(self, key):
+		e = self.Element(key)
+		if not key in self.tree.keys():self.tree[key] = e
+	def find(self, key):
+		if key in self.tree.keys():element = self.tree[key]
+		if key in self.tree.keys() and element.parent != element:element.parent  = self.find(element.parent.key)
+		if key in self.tree.keys():return element.parent
+	def union(self, element_a, element_b):
+		root_a = self.find(element_a.key);root_b = self.find(element_b.key)
+		if root_a != root_b and root_a.rank < root_b.rank:root_a.parent = root_b
+		elif root_a != root_b and root_a.rank > root_b.rank:root_b.parent = root_a
+		elif root_a != root_b:root_b.parent = root_a;root_a.rank+=1
+class Maze:
+	def __init__(self, width):
+		self.width = width;self.height = width;self.seed = random.random()*10000;self.grid = [[(width*row + col) for row in range(0,width)] for col in range(0, width)];self.portals = {};self.kruskalize()
+	def __str__(self):
+		s=''
+		for i in range(self.width):s+=str(random.randint(1,2))+str(random.randint(1,2))
+		s+=str(random.randint(1,2))+'\n'
+		for row in range(0,self.height):
+			s+=str(random.randint(1,2))+'0'
+			for col in range(1, self.width):s+='0'*(self.grid[col-1][row] in self.portals[self.grid[col][row]])+str(random.randint(1,2))*(self.grid[col-1][row] not in self.portals[self.grid[col][row]])+'0'
+			s+=str(random.randint(1,2))+'\n'
+			for col in range(0, self.width):key = self.grid[col][row];s+=str(random.randint(1,2))+'0'*(row+1 < self.height and self.grid[col][row+1] in self.portals[key])+str(random.randint(1,2))*(row+1 >= self.height or self.grid[col][row+1] not in self.portals[key])
+			s+=str(random.randint(1,2)) +'\n'
+		return s
+	def kruskalize(self):
+		edges_ordered = [ ]
+		for row in range(0, self.height):
+			for col in range(0, self.width):
+				cell = (col, row);left_cell = (col-1, row);down_cell = (col, row-1);near = []
+				if col > 0:near.append((left_cell, cell))
+				if row > 0:near.append( (down_cell, cell))
+				edges_ordered.extend(near)
+		random.seed(self.seed);edges = []
+		while len(edges_ordered) > 0:edges.append(edges_ordered.pop(random.randint(0,len(edges_ordered))-1))
+		disjoint_set = DisjointSet()
+		for row in range(0, self.height):
+			for col  in range(0,self.width):key = self.grid[col][row];disjoint_set.make_set(key);self.portals[key] = {}
+		edge_count = 0;key_count = self.grid[self.width-1][self.height-1]
+		while edge_count < key_count:
+			edge = edges.pop();key_a = self.grid[edge[0][0]][edge[0][1]];key_b = self.grid[edge[1][0]][edge[1][1]];set_a = disjoint_set.find(key_a);set_b = disjoint_set.find(key_b)
+			if set_a != set_b:edge_count+=1;self.portals[key_a][key_b] = True;self.portals[key_b][key_a] = True;disjoint_set.union(set_a, set_b)
+	def list(self):return [list(i) for i in str(self).split('\n')][:-1]
 class Maze3D:
-    def __init__(self, size):self.map=[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1],[1,0,1,0,1,1,1,0,1,0,2,2,0,2,0,1,1,0,1,0,1,1,1,0,1,0,1,0,0,1,0,1],[1,1,1,0,1,0,0,0,1,0,2,0,0,0,0,1,1,0,1,0,0,1,0,0,1,0,0,0,1,0,1,1],[1,0,0,0,0,0,1,1,1,0,2,0,2,1,1,1,0,0,0,1,0,1,0,1,1,1,1,0,1,0,1,1],[1,0,1,0,1,1,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1],[1,0,1,0,1,0,1,1,0,1,0,0,0,1,0,1,0,0,0,1,1,1,0,0,0,1,0,1,1,1,0,1],[1,1,1,0,1,0,0,1,0,2,2,2,2,2,0,1,1,0,1,0,0,1,1,1,1,1,0,1,0,1,1,1],[1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1],[1,1,1,1,0,1,1,1,1,0,0,2,0,2,1,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,0,1],[1,0,0,1,0,1,0,1,0,1,2,0,0,2,0,0,0,1,1,1,1,0,0,1,0,0,1,0,0,0,0,1],[1,1,0,1,0,1,0,0,0,2,0,0,2,0,0,1,0,0,0,1,1,1,1,1,1,1,1,0,1,1,1,1],[1,0,0,0,0,1,0,1,0,0,0,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,1,0,0,0,1,1],[1,1,0,1,1,0,1,1,0,1,1,1,0,1,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,1],[1,0,0,1,1,0,0,0,0,1,0,0,0,0,0,1,0,1,1,1,0,1,0,0,1,1,0,1,0,1,0,1],[1,0,1,1,0,0,1,1,1,0,0,1,1,0,1,1,0,0,0,1,1,1,0,0,0,1,0,1,0,0,0,1],[1,1,1,0,0,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,0,0,0,1,0,1,0,0,1,0,1,1],[1,0,0,0,1,1,1,1,0,0,0,1,0,0,1,0,1,1,0,1,1,1,0,1,0,1,1,0,1,0,0,1],[1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,1,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,1],[1,0,0,1,1,1,1,0,0,1,0,0,0,0,0,1,0,0,1,2,1,0,0,0,0,1,0,0,1,0,0,1],[1,1,0,0,1,0,0,1,0,1,0,1,1,1,0,1,0,1,1,0,1,0,1,0,1,1,0,1,1,1,0,1],[1,0,0,1,1,0,1,1,0,0,1,1,0,0,0,0,1,1,1,0,0,0,1,0,1,0,0,1,0,0,0,1],[1,0,1,1,0,0,0,1,1,0,1,0,0,1,1,0,0,0,1,1,1,1,0,1,1,0,1,0,0,1,1,1],[1,0,1,0,0,1,0,0,1,0,0,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1],[1,0,1,0,1,1,1,0,1,1,0,1,1,0,1,0,0,1,1,0,1,1,1,1,1,1,1,0,0,0,0,1],[1,0,1,0,1,0,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,1,0,0,0,0,1,1,1,0,1,1],[1,0,1,0,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,1,0,0,0,1,1,0,1,0,0,0,0,1],[1,0,1,0,0,0,1,1,0,0,0,1,0,1,1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,1],[1,0,1,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1],[1,0,0,0,1,1,0,1,1,1,0,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,1,1,0,1,0,1],[1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];app_path = os.path.dirname(os.path.abspath(__file__));assets_path = app_path + "/assets";self.size = size;self.width = self.size[0];self.height = self.size[1];self.walldark = [None];self.walllight = [None];self.walldark.append(pygame.image.load(assets_path + "/1.png").convert());self.walllight.append(pygame.image.load(assets_path + "/2.png").convert());self.walldark.append(pygame.image.load(assets_path + "/3.png").convert());self.walllight.append(pygame.image.load(assets_path + "/4.png").convert());self.textureSize = self.walldark[1].get_size();self.bg_img = pygame.image.load(assets_path + "/background.png").convert();self.bg_img = pygame.transform.scale(self.bg_img, size);self.playerX = 1.5;self.playerY = 1.5;self.playerDir = 0.0;self.playerStep = 0.05;self.angleStep = 5.0 * pi / 180.0;self.toggleTurnLeft = False;self.toggleTurnRight = False;self.toggleMoveFw = False;self.toggleMoveBw = False;self.defaultHeight = 400.0;self.numRays = NUMBER_OF_RAYS;self.viewAngle = 65 * pi / 180.0;self.rayAngleStep = self.viewAngle / (self.numRays - 1);self.columnWidth = self.width / (self.numRays - 1)
+    def __init__(self, size):self.map=Maze(42).list();app_path = os.path.dirname(os.path.abspath(__file__));assets_path = app_path + "/assets";self.size = size;self.width = self.size[0];self.height = self.size[1];self.walldark = [None];self.walllight = [None];self.walldark.append(pygame.image.load(assets_path + "/1.png").convert());self.walllight.append(pygame.image.load(assets_path + "/2.png").convert());self.walldark.append(pygame.image.load(assets_path + "/3.png").convert());self.walllight.append(pygame.image.load(assets_path + "/4.png").convert());self.textureSize = self.walldark[1].get_size();self.bg_img = pygame.image.load(assets_path + "/background.png").convert();self.bg_img = pygame.transform.scale(self.bg_img, size);self.playerX = 1.5;self.playerY = 1.5;self.playerDir = 0.0;self.playerStep = 0.05;self.angleStep = 5.0 * pi / 180.0;self.toggleTurnLeft = False;self.toggleTurnRight = False;self.toggleMoveFw = False;self.toggleMoveBw = False;self.defaultHeight = 400.0;self.numRays = NUMBER_OF_RAYS;self.viewAngle = 65 * pi / 180.0;self.rayAngleStep = self.viewAngle / (self.numRays - 1);self.columnWidth = self.width / (self.numRays - 1)
     def updatePos(self):
         if self.toggleTurnLeft:self.playerDir = (self.playerDir + self.angleStep) % (two_pi)
         if self.toggleTurnRight:self.playerDir = (self.playerDir - self.angleStep) % (two_pi)
