d4b39cd0dda90305933c38af211d774d95618c95
Elisha Hollander <just4now666666@gmail.com>
2020-11-03 16:14:38 +0200

    Update __init__.py

--- apps/tools/music/__init__.py
+++ apps/tools/music/__init__.py
@@ -18,10 +18,9 @@ class Mixer(object):
     @classmethod
     def get_volume(cls):return float(mixer.music.get_volume())
 class MusicPlayer(pygame_gui.elements.UIWindow):
-    DIMS = (400, 250)
     def __init__(self, pos, manager):
-        super().__init__(pygame.Rect(pos, (self.DIMS[0] + 32, self.DIMS[1] + 60)),manager=manager,window_display_title="MusicPlayer",object_id="#musicplayer",);self.mixer = Mixer();app_path = '\\'.join(os.path.dirname(os.path.realpath(__file__)).split('\\')[:-3]);self.musics_path = app_path + "/musics";self.musics = glob.glob(self.musics_path + "/*.ogg");self.musics_dict = dict((music_path.split("/")[-1], music_path) for music_path in self.musics);self.isPaused = False;btns_names = ["play", "pause", "stop", "+", "-"];self.BSIZE = (self.DIMS[0] / len(btns_names), 30)
-        for i, btn in enumerate(btns_names):self.play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((i % len(btns_names) * self.BSIZE[0], self.DIMS[1] - self.BSIZE[1]),self.BSIZE,),text=btn.upper(),manager=manager,container=self,object_id="#op-" + btn + "btn",);self.music_list = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((0, 0), (self.DIMS[0], self.DIMS[1] - self.BSIZE[1])),manager=manager,container=self,object_id="#op-musiclist",item_list=list(self.musics_dict.keys()),)
+        super().__init__(pygame.Rect(pos, (432, 310)),manager=manager,window_display_title="MusicPlayer",object_id="#musicplayer",);self.mixer = Mixer();app_path = '\\'.join(os.path.dirname(os.path.realpath(__file__)).split('\\')[:-3]);self.musics_path = app_path + "/musics";self.musics = glob.glob(self.musics_path + "/*.ogg");self.musics_dict = dict((music_path.split("/")[-1], music_path) for music_path in self.musics);self.isPaused = False;btns_names = ["play", "pause", "stop", "+", "-"];self.BSIZE = (400 / len(btns_names), 30)
+        for i, btn in enumerate(btns_names):self.play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((i % len(btns_names) * self.BSIZE[0], 250 - self.BSIZE[1]),self.BSIZE,),text=btn.upper(),manager=manager,container=self,object_id="#op-" + btn + "btn",);self.music_list = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((0, 0), (400, 250 - self.BSIZE[1])),manager=manager,container=self,object_id="#op-musiclist",item_list=list(self.musics_dict.keys()),)
     def process_event(self, event):
         super().process_event(event)
         if event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED and event.ui_object_id == "#musicplayer.#op-playbtn":selected_music = self.music_list.get_single_selection()
@@ -36,7 +35,4 @@ class MusicPlayer(pygame_gui.elements.UIWindow):
         if event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED:return True
     def update(self, delta):super().update(delta)
     def kill(self):self.mixer.stop();super().kill()
-def load(manager, params):
-    pos = (100, 100)
-    if params is not None and len(params) > 0:pos = params[0]
-    MusicPlayer(pos, manager)
+def load(manager, params):pos = params[0] if params is not None and len(params) > 0 else (100,100);MusicPlayer(pos, manager)
