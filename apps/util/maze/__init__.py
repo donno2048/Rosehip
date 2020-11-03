4cf1162346baf202aba18a2db992394233495362
Elisha Hollander <just4now666666@gmail.com>
2020-11-03 16:18:02 +0200

    Update __init__.py

--- apps/util/maze/__init__.py
+++ apps/util/maze/__init__.py
@@ -119,7 +119,4 @@ class Maze3DWindow(UIWindow):
     def __init__(self, position, ui_manager):super().__init__(pygame.Rect(position, (640, 480)),ui_manager,window_display_title="Maze 3D",object_id="#maze3d_window",);game_surface_size = self.get_container().get_size();self.game_surface_element = UIImage(pygame.Rect((0, 0), game_surface_size),pygame.Surface(game_surface_size).convert(),manager=ui_manager,container=self,parent_element=self,);self.maze3d = Maze3D(game_surface_size)
     def process_event(self, event):super().process_event(event);return self.maze3d.process_event(event)
     def update(self, time_delta):super().update(time_delta);self.maze3d.updatePos();self.maze3d.draw(self.game_surface_element.image)
-def load(manager, params):
-    pos = (100, 100)
-    if params is not None and len(params) > 0:pos = params[0]
-    Maze3DWindow(pos, manager)
+def load(manager, params):pos = params[0] if params is not None and len(params) > 0 else (100,100);Maze3DWindow(pos, manager)
