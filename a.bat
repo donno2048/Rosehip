29829ceff631008d2038cd19ae372b155898c481
Elisha Hollander <just4now666666@gmail.com>
2021-05-13 23:17:51 +0300

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,2 +1,1 @@
-rem "pip install roin" won't work, I can use the method I used in PyDonno (https://github.com/donno2048/PyDonno)
-pip install pygame pyttsx3 pywin32 comtypes pygame-gui keyboard wheel Js2Py selenium chromedriver-autoinstaller html2text requests opencv-python markdown2
+pip install roin
